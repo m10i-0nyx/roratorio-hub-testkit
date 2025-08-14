6a93a366d4807067c0cc8d94e397fb789bda5123
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-08-14 19:31:22 +0900

    [modify] Update tests/job_windhawk.spec.ts

--- tests/job_windhawk.spec.ts
+++ tests/job_windhawk.spec.ts
@@ -1,21 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('シミュレーション (ウインドホーク)', () => {
-    test('究極のラスガンドへのエイムドボルト攻撃', async ({ page, baseURL, browserName }) => {
+    test('「ベテルギウス」への「エイムドボルト」攻撃', async ({ page, baseURL, browserName }) => {
         // ページに移動
         await page.goto(
-            `${baseURL}?cx1cy1xtHmpseEoA2pb100wA91cz21001_bQBqcpHsanjD5Kjy7cz22.419hcz23001UjsyxVGsDOltcz24001-hqRaqyBFwkAOj2cz25001-hf5YmjrPwDu6-lcz26001VjiS1h93o_86Cvcz27001UjfFpNvrv3Opcz28001VjszhODVJxCjG4cz29001-h8usaldNabe2Vocz2a001-he5smrfPoIf6ldcz2b.4hoicz2c.4hpacA1Z_1127456b89a3cA128c0cB1.m1Mvhf87Z-__-DxGGC1l5515h5B6GmBmqlG6jFll1l5cC1.ecD1.fxKfaaaaaaaaa5cR2.4124cW100dvx2001cZ1i2`,
+            `${baseURL}?cx1cy1xtHmpseEoA2pb100wA91cz21001kbQBqylsly7cz22.419hcz23001gjs2LnFn5cz24001khqlUR1Ej2cz25001khf5AX2e-lcz26001hjiS1hpfmr7cz27001gjfFpSvb1cz28001hjszhOLEiE4cz29001kh8uldkmVocz2a001khe5CvOpldcz2b.4hoicz2c.4hpacA1Z_1127456b89a3cA128c0cB1.m1Mvhf87Z-__-DxGGC1l5515h5a5B6GmBmqlG6jFll1l5cC1.ecD1.fxKfaaaaaaaaa5cR2.41QfcW100dvx2001cZ1i2`
         );
 
         // 要素に対する操作
-        await page.getByRole('cell', { name: '通常攻撃', exact: true }).getByRole('combobox').selectOption('497'); //エイムドボルト選択
-        await page.getByRole('textbox', { name: 'Jガーディアン-P' }).click();
-        await page.getByRole('option', { name: '究極のラスガンド' }).click();
         await page.getByRole('button', { name: '計算する' }).click();
 
         // ダメージ検証
-        await expect(page.getByText('2095032', { exact: true }).first()).toBeVisible(); // 最小ダメージ
-        await expect(page.getByText('1717271', { exact: true }).first()).toBeVisible(); // 平均ダメージ
-        await expect(page.getByText('2618790', { exact: true }).first()).toBeVisible(); // 最大ダメージ
+        await expect(page.getByText('9223780', { exact: true }).first()).toBeVisible(); // 最小ダメージ
+        await expect(page.getByText('8972253', { exact: true }).first()).toBeVisible(); // 平均ダメージ
+        await expect(page.getByText('11524830', { exact: true }).first()).toBeVisible(); // 最大ダメージ
     });
 });
